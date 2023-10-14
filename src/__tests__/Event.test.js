08baaa9455d771c5f4acab544daff2414df75cb6
Alex Sweeney <ajsweeney2324@gmail.com>
2023-10-14 14:09:24 -0500

    tests to show and hide details

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import Event from '../components/Event';
 import mockData from '../mock-data';
+import userEvent from '@testing-library/user-event';
 
 
 const mockEvent = mockData[2];
@@ -22,5 +23,31 @@ describe('<Event /> Component', () => {
     test('Has Event Location', () => {
         expect(EventComponent.queryByText(mockEvent.location)).toBeInTheDocument();
     })
+
+    test('By Default, Event Details are Hidden', () => {
+        expect(EventComponent.queryByText(mockEvent.description)).not.toBeInTheDocument();
+    })
+
+    test("When a user clicks 'Show Details', the details are shown", async () => {
+        const user = userEvent.setup();
+        const button = EventComponent.queryByText('Show Details');
+        await user.click(button);
+
+        const eventDOM = EventComponent.container.firstChild;
+        const details = eventDOM.querySelector('.details');
+        expect(details).toBeInTheDocument();
+    })
+
+    test('hide details after user clicks button "hide details"', async () => {
+        const button = EventComponent.queryByText('Show Details');
+        const eventDOM = EventComponent.container.firstChild;
+        await userEvent.click(button);
+    
+        const hideButton = EventComponent.queryByText('Hide Details');
+        await userEvent.click(hideButton);
+    
+        const details = eventDOM.querySelector('.details');
+        expect(details).not.toBeInTheDocument();
+      });
 
 })
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,23 @@
-
+import { useState } from 'react';
 
 const Event = ({ event }) => {
+    const [showDetails, setShowDetails] = useState(false);
     return(
         <li className="event">
             <h3>{event.summary}</h3>
             <p>{event.start?.dateTime || 'Start Time Not Provided'}</p>
             <p>{event.location}</p>
+            <button
+                className='show-hide-details'
+             onClick={() => setShowDetails(!showDetails)}>
+                {showDetails ? 'Hide Details' : "Show Details"}
+            </button>
+            {showDetails ? (
+                <div className='details'>
+                    <h4>Event Details</h4>
+                    <p>Description: {event.description}</p>
+                </div>
+            ) : null}
         </li>
     );
 }
