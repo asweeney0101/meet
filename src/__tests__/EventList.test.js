ff6a78a8b74f5110146045251e406b221fc9d2a4
Alex Sweeney <ajsweeney2324@gmail.com>
2023-10-14 16:13:07 -0500

    removed 4 event mock data

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -6,17 +6,14 @@ describe('<EventList /> component', () => {
 
     let EventListComponent;
     beforeEach(() => {
-      EventListComponent = render(<EventList events={[{ id: 1 },{ id: 2 },{ id: 3 },{ id: 4 }]}/>);
+      EventListComponent = render(<EventList />);
     })
 
     test('has an element with "list" role', () => {
       expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
     });
 
-    test('has an element with "list" role', () => {
-      expect (EventListComponent.getAllByRole("listitem").length).toBe(4);
-    })
-    
+       
     test('renders correct number of events', async () => {
       const allEvents = await getEvents(); 
       EventListComponent.rerender(<EventList events={allEvents} />);
