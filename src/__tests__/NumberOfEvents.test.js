d7d4cd6c7084a4d461ba61b6923e9c197cf3a860
Alex Sweeney <ajsweeney2324@gmail.com>
2023-10-14 17:37:17 -0500

    updating number of events and test

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import NumberOfEvents from '../components/NumberOfEvents';
+import userEvent from '@testing-library/user-event';
 
 describe('<NumberOfEvents /> Component', () => {
     let NOEComponent;
@@ -14,4 +15,11 @@ describe('<NumberOfEvents /> Component', () => {
     test('default value of textbox is 32', () => {
         expect(NOEComponent.queryByRole('textbox')).toHaveValue('32');
     })
+
+    test('updates number of events when user types', async () => {
+        const input = NOEComponent.queryByRole('textbox');
+        await userEvent.type(input, '{backspace}{backspace}10');
+        expect(input).toHaveValue('10');
+      });
+
 })
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
 const NumberOfEvents = () => {
+    const [value, setValue] = useState('32');
     return(
         <div id="number-of-events">
            <input
                 type="text"
-                defaultValue={32}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
            />
         </div>
     )
