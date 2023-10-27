6cc325e1e98de9dd623d4ff2f4f7803e13ce83d6
Alex Sweeney <ajsweeney2324@gmail.com>
2023-10-26 22:18:09 -0500

    updating getevents to use google api

--- src/api.js
+++ src/api.js
@@ -6,6 +6,42 @@ export const extractLocations = (events) => {
     return locations;
 }
 
+const checkToken = async (accessToken) => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    const result = await response.json();
+    return result;
+  };
+
 export const getEvents = async () => {
+     if (window.location.href.startsWith('http://localhost')) {
     return mockData;
+  }
 }
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+          const response = await fetch(
+            "https://uzn77hnpci.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+          );
+          const result = await response.json();
+          const { authUrl } = result;
+          return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+      }
+      return accessToken;
+
+
+      
+    
+};
+
