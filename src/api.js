e0737607f328cf4347f48dfb35d29d2adb1fa52f
Alex Sweeney <ajsweeney2324@gmail.com>
2023-10-28 15:14:04 -0500

    adding auth to get live events

--- src/api.js
+++ src/api.js
@@ -14,11 +14,39 @@ const checkToken = async (accessToken) => {
     return result;
   };
 
-export const getEvents = async () => {
-     if (window.location.href.startsWith('http://localhost')) {
-    return mockData;
-  }
-}
+  export const getEvents = async () => {
+    if (window.location.href.startsWith("http://localhost")) {
+      return mockData;
+    }
+  
+    const token = await getAccessToken();
+    
+    const removeQuery = () => {
+        let newurl;
+        if (window.history.pushState && window.location.pathname) {
+          newurl =
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname;
+          window.history.pushState("", "", newurl);
+        } else {
+          newurl = window.location.protocol + "//" + window.location.host;
+          window.history.pushState("", "", newurl);
+        }
+      };
+
+    if (token) {
+      removeQuery();
+      const url =  "https://uzn77hnpci.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+      const response = await fetch(url);
+      const result = await response.json();
+      if (result) {
+        return result.events;
+      } else return null; 
+    }
+  };
+
 
 export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
@@ -39,9 +67,16 @@ export const getAccessToken = async () => {
         return code && getToken(code);
       }
       return accessToken;
-
-
-      
-    
+        
 };
 
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+      'https://uzn77hnpci.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    );
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+  
+    return access_token;
+  };
