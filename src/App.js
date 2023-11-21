9c1b699ee4644a37093d9935c1caca8a6cfa0d43
Alex Sweeney <ajsweeney2324@gmail.com>
2023-11-20 19:58:35 -0600

    adding alert classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { InfoAlert } from './components/Alert';
 import { extractLocations, getEvents } from './api'
 import { useEffect, useState } from 'react';
+
 
 import './App.css';
 
@@ -16,6 +18,7 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
   
   useEffect(() => {
     fetchData();
@@ -32,6 +35,9 @@ const App = () => {
 
   return (
    <div className="App">
+        <div className="alerts-container">
+           {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        </div>
      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
      <EventList events={events}/>
