1d75e404b81e3b20c7223ac7745179641756c920
Alex Sweeney <ajsweeney2324@gmail.com>
2023-12-12 21:11:02 -0600

    importing and adding recharts

---
+++ src/components/CityEventChart.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = () => {
+}
+
+export default CityEventsChart;
+
