5a8688a2dcf454b3757928117d9b6e853399839f
Alex Sweeney <ajsweeney2324@gmail.com>
2023-12-28 19:50:54 -0600

    test simple deploy

--- src/App.js
+++ src/App.js
@@ -14,72 +14,72 @@ import './App.css';
 
 const App = () => {
 
-  const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
-  const [allLocations, setAllLocations] = useState([]);
-  const [currentCity, setCurrentCity] = useState("See all cities");
-  const [infoAlert, setInfoAlert] = useState("");
-  const [errorAlert, setErrorAlert] = useState("");
-  const [warningAlert, setWarningAlert] = useState("");
+//   const [events, setEvents] = useState([]);
+//   const [currentNOE, setCurrentNOE] = useState(32);
+//   const [allLocations, setAllLocations] = useState([]);
+//   const [currentCity, setCurrentCity] = useState("See all cities");
+//   const [infoAlert, setInfoAlert] = useState("");
+//   const [errorAlert, setErrorAlert] = useState("");
+//   const [warningAlert, setWarningAlert] = useState("");
   
-  useEffect(() => {
-    if (navigator.onLine) {
-      setWarningAlert("")
-    } else {
-      setWarningAlert("You are Currently Navigating Offline")
-    }
-    fetchData();
-  }, [currentCity, currentNOE]);
+//   useEffect(() => {
+//     if (navigator.onLine) {
+//       setWarningAlert("")
+//     } else {
+//       setWarningAlert("You are Currently Navigating Offline")
+//     }
+//     fetchData();
+//   }, [currentCity, currentNOE]);
   
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ? 
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
+//   const fetchData = async () => {
+//     const allEvents = await getEvents();
+//     const filteredEvents = currentCity === "See all cities" ? 
+//       allEvents :
+//       allEvents.filter(event => event.location === currentCity)
+//     setEvents(filteredEvents.slice(0, currentNOE));
+//     setAllLocations(extractLocations(allEvents));
+//   }
 
-  return (
-   <div className="App">
+//   return (
+//    <div className="App">
       
-        <div className="alerts-container">
-           {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
-           {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
-           {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
-        </div>
+//         <div className="alerts-container">
+//            {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+//            {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+//            {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
+//         </div>
 
-        <div className="title-container">
-            <h1>Meet App</h1>
-            <p>Grow your knowledge by finding tech events near you!</p>
-        </div>
+//         <div className="title-container">
+//             <h1>Meet App</h1>
+//             <p>Grow your knowledge by finding tech events near you!</p>
+//         </div>
 
-        <div>
-            <CitySearch 
-              allLocations={allLocations} 
-              setCurrentCity={setCurrentCity} 
-              setInfoAlert={setInfoAlert}
-           />  
-          <div className="noe-container">
-            <p># of Events: </p>
-            <NumberOfEvents 
-              setCurrentNOE={setCurrentNOE}
-              setErrorAlert={setErrorAlert}
-             />
-          </div>
-        </div>
+//         <div>
+//             <CitySearch 
+//               allLocations={allLocations} 
+//               setCurrentCity={setCurrentCity} 
+//               setInfoAlert={setInfoAlert}
+//            />  
+//           <div className="noe-container">
+//             <p># of Events: </p>
+//             <NumberOfEvents 
+//               setCurrentNOE={setCurrentNOE}
+//               setErrorAlert={setErrorAlert}
+//              />
+//           </div>
+//         </div>
 
-        <div className="charts-container">
-          <EventGenresChart events={events} />
-          <CityEventsChart allLocations={allLocations} events={events} />    
-        </div>       
+//         <div className="charts-container">
+//           <EventGenresChart events={events} />
+//           <CityEventsChart allLocations={allLocations} events={events} />    
+//         </div>       
 
-        <EventList allLocations={allLocations} events={events}/>
+//         <EventList allLocations={allLocations} events={events}/>
 
-   </div>
+//    </div>
    
- );
- 
+//  );
+  return (<div>test text</div>)
 
 }
 
